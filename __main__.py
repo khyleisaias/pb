230b74349595329661b4b4eea50ee6f6ddc2da13
kernelk14 <khyleisaias@gmail.com>
2022-08-01 21:39:24 +0800

    PB: 'cat' a file
    you know, i 'cat' a file inside python.
    i'm currently making a test for files that exists.
    
    *= Little Knowledge =*
    'cat' utility in linux, basically reading the file.

--- __main__.py
+++ __main__.py
@@ -6,17 +6,20 @@ import subprocess
 """ PB: A New Build System """
 """ NOTE: Pybuild is a failed project, so I'm making a new one. """
 ip = 0
-
-
-
+def cat(cur_file):
+    with open(cur_file, 'r') as f:
+        return f.read()
 def files(file_list):
     global ip
     fl = file_list.split(" ")
     for ip in range(len(fl)):
         if os.path.exists(fl[ip]):
+            cur_file = fl[ip]
             print(f"`{fl[ip]}` exist, you can use the file")
+            print(cat(cur_file))
         else:
             print(f"`{fl[ip]}` does not exist, you cannot continue")
+            exit(1)
         ip += 1
     ip += 1
 
